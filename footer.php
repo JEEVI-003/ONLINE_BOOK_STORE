c1acb89e4f32426d68bb25a905eca63cffc7761e
JEEVI-003 <snjeevikesh@gmail.com>
2025-09-24 12:29:53 +0530

    commit

---
+++ footer.php
@@ -0,0 +1,84 @@
+
+
+<footer class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Books</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="wishlist.php"> <i class="fas fa-angle-right"></i> Wishlist</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i> Register</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91-93615-93789 </p>
+         <p> <i class="fas fa-phone"></i> +91-93454-04940 </p>
+         <p> <i class="fas fa-envelope"></i> naganandhini3012@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Madurai, india - 625005 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/naganandhini-n-k-954214231"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </section>
+
+   <!-- icons section starts  -->
+
+<section class="icons-container">
+
+<div class="icons">
+    <i class="fas fa-shipping-fast"></i>
+    <div class="content">
+        <h3>free shipping</h3>
+        <p>order over $100</p>
+    </div>
+</div>
+
+<div class="icons">
+    <i class="fas fa-lock"></i>
+    <div class="content">
+        <h3>secure payment</h3>
+        <p>100 secure payment</p>
+    </div>
+</div>
+
+<div class="icons">
+    <i class="fas fa-redo-alt"></i>
+    <div class="content">
+        <h3>easy returns</h3>
+        <p>10 days returns</p>
+    </div>
+</div>
+
+<div class="icons">
+    <i class="fas fa-headset"></i>
+    <div class="content">
+        <h3>24/7 support</h3>
+        <p>call us anytime</p>
+    </div>
+</div>
+
+</section>
+
+<!-- icons section ends -->
+
+
+   <p class="credit"> &copy; copyright @ <?= date('Y'); ?> by <span>Nandhini</span> </p>
+
+</footer>
